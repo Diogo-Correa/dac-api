c282a357c854952872636efd646fe9a1612bdab3
diogocorrea <diogocorrea@id.uff.br>
2023-10-24 17:45:09 -0300

    user model

---
+++ src/main/java/com/dac/api/app/model/user/User.java
@@ -0,0 +1,31 @@
+package com.dac.api.app.model.user;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String username;
+    private String email;
+
+    public User(
+            String username,
+            String email) {
+        this.username = username;
+        this.email = email;
+    }
+}
